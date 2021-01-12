ed83b9a1cb12b459e2a8c6a18273024206ce6b5a
zhaolu <zhaolu16120284@163.com>
2021-01-12 13:10:41 +0800

    添加__getitem__魔法方法

--- object_oriented/magic_methods.py
+++ object_oriented/magic_methods.py
@@ -17,3 +17,18 @@ print(foo.__module__)
 print(foo) #自动调用__call__方法
 print(Foo.__dict__)
 #__getitem__
+
+class MyData():
+    def __init__(self):
+        self.nums = [1,2,3,4,5,6,7,8]
+
+    def __getitem__(self, item):
+        return self.nums[item]
+
+    def __len__(self):
+        return len(self.nums)
+
+
+mydata = MyData()
+print(mydata[1])
+print(len(mydata))
