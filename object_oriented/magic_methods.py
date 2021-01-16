6503e74cfde927f28756df5157ae948f06f0f84f
zhaolu <zhaolu16120284@163.com>
2021-01-16 15:04:07 +0800

    添加__all__魔法方法

--- object_oriented/magic_methods.py
+++ object_oriented/magic_methods.py
@@ -16,8 +16,9 @@ print(foo.__class__)
 print(foo.__module__)
 print(foo) #自动调用__call__方法
 print(Foo.__dict__)
-#__getitem__
 
+
+#__getitem__
 class MyData():
     def __init__(self):
         self.nums = [1,2,3,4,5,6,7,8]
@@ -32,3 +33,8 @@ class MyData():
 mydata = MyData()
 print(mydata[1])
 print(len(mydata))
+
+
+#__all__(用于限制模块导入)
+#使用from model import *, 此时只能导入resnet18, Resnet, resnet50
+__all__ = ['resnet18', 'Resnet', 'resnet50']
